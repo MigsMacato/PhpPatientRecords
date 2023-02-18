b3c4c1767db20e2f3277d318e7fec82cc14caa52
MigsMacato <migsmacato08@gmail.com>
2023-02-18 20:53:30 +0800

    Upadate Database

--- system/core/URI.php
+++ system/core/URI.php
@@ -49,7 +49,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
  * @link		https://codeigniter.com/user_guide/libraries/uri.html
  */
 class CI_URI {
-
+	/**
+ 	* CI Config
+ 	*
+ 	* @var CI_Config
+ 	*/
+	public $config;
 	/**
 	 * List of cached URI segments
 	 *
