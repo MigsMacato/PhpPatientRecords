eac4eda037e2586efedd5457bf33c93853dd03ea
Miguel Macato <migs@Miguels-MBP-2.lan>
2023-02-22 17:17:37 +0800

    Test

--- application/views/dashboard/homedashboard_view.php
+++ application/views/dashboard/homedashboard_view.php
@@ -18,7 +18,7 @@
                     <div class="col mr-2">
                       <div class="h3 mb-0 font-weight-bold text-gray-800">
                     
-                      <?php echo "&nbsp&nbsp&nbsp&nbsp&nbsp <a class='text-secondary' href='". base_url()."admissioncontrol/admitdatatable'>"."<div>".$get_data."</div>"."</a>" ?>
+                      <?php echo "&nbsp&nbsp&nbsp&nbsp&nbsp <a class='text-secondary' href='". base_url()."admissioncontrol/admitdatatable'>"."<br>".$get_data."</br>"."</a>" ?>
                          
                       </div>
                     </div>
