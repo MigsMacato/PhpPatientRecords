b28797cca95c52141b409cecca4c74db0dee7ca7
Miguel Macato <migs@Miguels-MacBook-Pro-2.local>
2023-02-22 21:55:27 +0800

    Test

--- application/views/dashboard/homedashboard_view.php
+++ application/views/dashboard/homedashboard_view.php
@@ -38,7 +38,7 @@
                        <div class="col mr-2">
                       <div class="h3 mb-0 font-weight-bold text-gray-800">
                         
-                       <?php echo "&nbsp&nbsp&nbsp&nbsp&nbsp <a class='text-secondary' href='". base_url()."addusercontrol/adduserview'>"."<br>".$get_user."</brv>"."</a>" ?>
+                       <?php echo "&nbsp&nbsp&nbsp&nbsp&nbsp <a class='text-secondary' href='". base_url()."addusercontrol/adduserview'>"."<br>"."</br>"."</a>" ?>
                         
 
                       </div>
@@ -60,7 +60,7 @@
                        <div class="col mr-2">
                       <div class="h3 mb-0 font-weight-bold text-gray-800">
                         
-                       <?php echo "&nbsp&nbsp&nbsp&nbsp&nbsp <a class='text-secondary' href='". base_url()."addusercontrol/adddoctorview'>"."<br>".$get_doctors."</br>"."</a>" ?>
+                       <?php echo "&nbsp&nbsp&nbsp&nbsp&nbsp <a class='text-secondary' href='". base_url()."addusercontrol/adddoctorview'>"."<br>"."</br>"."</a>" ?>
                         
                       </div>
                     </div>   
