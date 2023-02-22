5e9975bb17fd24d50cff4adc09a83ab87f42a65a
Miguel Macato <migs@Miguels-MBP-2.lan>
2023-02-22 16:19:17 +0800

    Test

--- application/views/layouts/central_template.php
+++ application/views/layouts/central_template.php
@@ -104,7 +104,7 @@
 
       <!-- Nav Item - Dashboard -->
       <li class="nav-item active">
-        <a class="nav-link" href="<?php echo base_url(); ?>usercontrol/index">">
+        <a class="nav-link" href="<?php echo base_url(); ?>usercontrol/index">
           <i class="fas fa-fw fa-tachometer-alt"></i>
           <span>Dashboard</span></a>
       </li>
