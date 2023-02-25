accb12074fae6594fe38954c3502a194c859dd6c
Miguel Macato <migs@Miguels-MBP-2.lan>
2023-02-25 08:47:17 +0800

    Testing new Function

--- application/views/layouts/central_template.php
+++ application/views/layouts/central_template.php
@@ -195,13 +195,13 @@
           <span>Add Users</span></a>
       </li>
 
-
+<!--
       <li class="nav-item active">
-         <!-- Nav Item - Tables -->
+          Nav Item - Tables 
         <a class="nav-link collapsed " href="<?php echo base_url(); ?>logscontrol/logsview">
           <i class="fas fa-fw  fa-users "></i>
           <span>User Logs</span></a>
-      </li>
+      </li>-->
 
       <!-- Divider -->
       <hr class="sidebar-divider d-none d-md-block">
