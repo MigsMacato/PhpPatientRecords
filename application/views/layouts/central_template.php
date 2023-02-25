03a38ed832f9c68ae3ec9234bc5fc64efe3b44d8
Miguel Macato <migs@Miguels-MBP-2.lan>
2023-02-25 09:05:07 +0800

    Testing new Function

--- application/views/layouts/central_template.php
+++ application/views/layouts/central_template.php
@@ -91,7 +91,7 @@
     <ul class=" navbar-nav bg-gradient-success sidenav sidebar sidebar-dark accordion " id="accordionSidebar">
 
       <!-- Sidebar - Brand -->
-      <a class="sidebar-brand d-flex align-items-center justify-content-center"  href="#">
+      <a class="sidebar-brand d-flex align-items-center justify-content-center"  href="admissioncontrol/admitdatatable">
         <div class="sidebar-brand-icon rotate-n-0">
            <i class="fas fa-folder-open"></i>
         </div>
