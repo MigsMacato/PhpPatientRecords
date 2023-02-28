a8251d20d7f8087f7e376975003e97b31b825865
MigsMacato <migsmacato08@gmail.com>
2023-02-28 21:47:55 +0800

    Functions

--- application/views/dashboard/homedashboard_view.php
+++ application/views/dashboard/homedashboard_view.php
@@ -38,7 +38,7 @@
                        <div class="col mr-2">
                       <div class="h3 mb-0 font-weight-bold text-gray-800">
                         
-                       <?php echo "&nbsp&nbsp&nbsp&nbsp&nbsp <a class='text-secondary' href='". base_url()."#'>"."".$this->db->count_all_results('standardusers','users')."</a>" ?>
+                       <?php echo "&nbsp&nbsp&nbsp&nbsp&nbsp <a class='text-secondary' href='". base_url()."#'>"."".$this->db->count_all_results('standardusers')."</a>" ?>
                         
 
                       </div>
@@ -60,7 +60,7 @@
                        <div class="col mr-2">
                       <div class="h3 mb-0 font-weight-bold text-gray-800">
                         
-                       <?php echo "&nbsp&nbsp&nbsp&nbsp&nbsp <a class='text-secondary' href='". base_url()."addusercontrol/adddoctorview'>"."".$get_doctor.""."</a>" ?>
+                       <?php echo "&nbsp&nbsp&nbsp&nbsp&nbsp <a class='text-secondary' href='". base_url()."addusercontrol/adddoctorview'>"."".$this->db->count_all_results('physicians').""."</a>" ?>
                         
                       </div>
                     </div>   
