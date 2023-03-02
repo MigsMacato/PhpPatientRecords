2d8bf4f33a73be0e97705e19283cd020405ab602
MigsMacato <migsmacato08@gmail.com>
2023-03-02 20:27:15 +0800

    Functions

--- application/views/admission/opdform.php
+++ application/views/admission/opdform.php
@@ -108,7 +108,7 @@
                      <div class="text-danger text-center"><?php echo form_error('email'); ?></div> 
                    </div>
 
-                    <div style="margin-left: 20px;" class="col-xs-3">
+                    <div style="margin-left: 20px;" class="col-xs-12">
                       <?php echo form_label('Known Illness'); ?>
                                 <input type="text" class="form-control <?= (form_error('illness') == "" ? '':'is-invalid') ?>" placeholder="Illness"  name="illness" value="<?php echo set_value('illness') ?>">
                      <div class="text-danger text-center"><?php echo form_error('illness'); ?></div>
