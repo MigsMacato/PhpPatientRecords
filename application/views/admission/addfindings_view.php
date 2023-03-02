a404933421dee0417f6c0bf482a7e2158ce6e668
Miguel Macato <migs@Miguels-MBP-2.lan>
2023-03-02 14:40:14 +0800

    Testing new Function

--- application/views/admission/addfindings_view.php
+++ application/views/admission/addfindings_view.php
@@ -162,13 +162,14 @@
 
                 </div>
               </div>
+              <!--
                <div class="card shadow mb-4">
-                <!-- Card Header - Dropdown -->
+                Card Header - Dropdown 
                 <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
                   <h6 class="m-0 font-weight-bold text-success">Admission <?php echo "<a style='margin-left: 550px; text-decoration:none;' class='text-secondary' href='". base_url() ."admissioncontrol/admit_form/". $get_data->pr_id ."#admission'>" ?> <i class="fa 
                    fa-plus"></i></a></h6>
                 </div>
-                <!-- Card Body -->
+                Card Body 
                 <div class="card-body">
                  
                   
@@ -201,7 +202,7 @@
               </div>
 
                 </div>
-              </div>
+              </div>-->
             </div>
  
      
