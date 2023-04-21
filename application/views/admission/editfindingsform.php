6a3199f08b7daf7563b93ef34ee23de99a143eca
Miguel Macato <migs@Miguels-MBP-2.lan>
2023-04-21 14:35:06 +0800

    Requirements

--- application/views/admission/editfindingsform.php
+++ application/views/admission/editfindingsform.php
@@ -30,7 +30,7 @@
           <input class="form-control <?= (form_error('e_bp') == "" ? '':'is-invalid') ?>" type="text" name="e_bp" value="<?php echo $get_findings_view->f_bp; ?>" placeholder="BP">            
           <div class="text-danger"><?php echo form_error('e_bp'); ?></div> 
           </div>
-           <div class="col-sm-4">
+           <div class="col-sm-4"style="margin-bottom:17px;">
           <?php echo form_label('Respiratory Rate'); ?>
           <input class="form-control<?= (form_error('e_rr') == "" ? '':'is-invalid') ?>" type="text" name="e_rr" value="<?php echo $get_findings_view->f_rr; ?>" placeholder="RR">  
           <br>
